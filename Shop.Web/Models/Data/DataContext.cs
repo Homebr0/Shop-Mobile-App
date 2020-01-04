4c8a93bcd8787042019d185d30cba3a3664a4c1d
Jason Davila <jsndavila1@gmail.com>
2020-01-04 15:42:18 -0500

    oh boy

---
+++ Shop.Web/Models/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Shop.Web.Models.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Shop.Web.Models.Data.Entities;
+
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}

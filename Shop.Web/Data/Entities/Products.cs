38c8ff775f4c4c3708e740af35c5e3f8e2020e84
Jason Davila <jsndavila1@gmail.com>
2020-01-06 18:40:43 -0500

    Added user identities so there can exist an administrative role as well as a customer or user role. Also added new User table in the DB linked to product via foreign key. The link is to restrict how users are able to interact witha product based on their identity.

--- Shop.Web/Data/Entities/Products.cs
+++ Shop.Web/Data/Entities/Products.cs
@@ -30,6 +30,8 @@ namespace Shop.Web.Data.Entities
 
         [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
         public double Stock { get; set; }
+
+        public User User { get; set; }
     }
 
 }
--- Shop.Web/Data/SeedDb.cs
+++ Shop.Web/Data/SeedDb.cs
@@ -6,15 +6,18 @@ namespace Shop.Web.Data
     using System.Linq;
     using System.Threading.Tasks;
     using Entities;
+    using Microsoft.AspNetCore.Identity;
 
     public class SeedDb
     {
         private readonly DataContext context;
+        private readonly UserManager<User> userManager;
         private Random random;
 
-        public SeedDb(DataContext context)
+        public SeedDb(DataContext context, UserManager<User> userManager)
         {
             this.context = context;
+            this.userManager = userManager;
             this.random = new Random();
         }
 
@@ -22,25 +25,46 @@ namespace Shop.Web.Data
         {
             await this.context.Database.EnsureCreatedAsync();
 
+            var user = await this.userManager.FindByEmailAsync("jzuluaga55@gmail.com");
+            if (user == null)
+            {
+                user = new User
+                {
+                    FirstName = "Jason",
+                    LastName = "Davila",
+                    Email = "jdavi247@fiu.edu",
+                    UserName = "Administrator",
+                    PhoneNumber = "3055033005"
+                };
+
+                var result = await this.userManager.CreateAsync(user, "123456");
+                if (result != IdentityResult.Success)
+                {
+                    throw new InvalidOperationException("Could not create the user in seeder");
+                }
+            }
+
             if (!this.context.Products.Any())
             {
-                this.AddProduct("iPhone X");
-                this.AddProduct("Magic Mouse");
-                this.AddProduct("Apple Watch Series 7");
+                this.AddProduct("iPhone Q", user);
+                this.AddProduct("Macbook Bro", user);
+                this.AddProduct("Potted Cactus", user);
+                this.AddProduct("Hand Sanitizer", user);
                 await this.context.SaveChangesAsync();
             }
         }
 
-        private void AddProduct(string name)
+
+        private void AddProduct(string name, User user)
         {
             this.context.Products.Add(new Product
             {
                 Name = name,
                 Price = this.random.Next(1000),
                 IsAvailabe = true,
-                Stock = this.random.Next(100)
+                Stock = this.random.Next(100),
+                User = user
             });
         }
     }
-
 }

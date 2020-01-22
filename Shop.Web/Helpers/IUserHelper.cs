619abddf55ce3b6e125cac3baee88907c104d0ad
Jason Davila <jsndavila1@gmail.com>
2020-01-22 14:42:01 -0500

    Created a login menu and user authorization to access the shop. This commit does not have a means of creating a user without direct access to the database, so that will be implemented with the following commit.

--- Shop.Web/Helpers/IUserHelper.cs
+++ Shop.Web/Helpers/IUserHelper.cs
@@ -5,11 +5,17 @@ namespace Shop.Web.Helpers
     using System.Threading.Tasks;
     using Data.Entities;
     using Microsoft.AspNetCore.Identity;
+    using Shop.Web.Models;
 
     public interface IUserHelper
     {
         Task<User> GetUserByEmailAsync(string email);
 
         Task<IdentityResult> AddUserAsync(User user, string password);
+
+        Task<SignInResult> LoginAsync(LoginViewModel model);
+
+        Task LogoutAsync();
+
     }
 }
--- Shop.Web/Helpers/UserHelper.cs
+++ Shop.Web/Helpers/UserHelper.cs
@@ -4,16 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Shop.Web.Data.Entities;
+using Shop.Web.Models;
 
 namespace Shop.Web.Helpers
 {
     public class UserHelper : IUserHelper
     {
         private readonly UserManager<User> userManager;
+        private readonly SignInManager<User> signInManager;
 
-        public UserHelper(UserManager<User> userManager)
+        public UserHelper(UserManager<User> userManager, SignInManager<User> signInManager)
         {
             this.userManager = userManager;
+            this.signInManager = signInManager;
         }
 
         public async Task<IdentityResult> AddUserAsync(User user, string password)
@@ -24,6 +27,20 @@ namespace Shop.Web.Helpers
         public async Task<User> GetUserByEmailAsync(string email)
         {
             return await this.userManager.FindByEmailAsync(email);
+        }
+
+        public async Task<SignInResult> LoginAsync(LoginViewModel model)
+        {
+            return await this.signInManager.PasswordSignInAsync(
+                model.Username,
+                model.Password,
+                model.RememberMe,
+                false);
+        }
+
+        public async Task LogoutAsync()
+        {
+            await this.signInManager.SignOutAsync();
         }
     }
 }

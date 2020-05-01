1e10b383d801019b6cb4a4955f84762ca5904fbe
Jason Davila <jsndavila1@gmail.com>
2020-05-01 18:31:21 -0400

    OrderRepository fetches orders in our order table based on role, admin or user

---
+++ Shop.Web/Data/Repositories/OrderRepository.cs
@@ -0,0 +1,46 @@
+﻿
+
+namespace Shop.Web.Data.Repositories
+{
+    using Entities;
+    using Microsoft.EntityFrameworkCore;
+    using Shop.Web.Helpers;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class OrderRepository : GenericRepository<Order>, IOrderRepository
+    {
+        private readonly DataContext context;
+        private readonly IUserHelper userHelper;
+
+        public OrderRepository(DataContext context, IUserHelper userHelper) : base(context)
+        {
+            this.context = context;
+            this.userHelper = userHelper;
+        }
+
+        public async Task<IQueryable<Order>> GetOrdersAsync(string userName)
+        {
+            var user = await this.userHelper.GetUserByEmailAsync(userName);
+            if (user == null)
+            {
+                return null;
+            }
+
+            if (await this.userHelper.IsUserInRoleAsync(user, "Admin"))
+            {
+                return this.context.Orders
+                    .Include(o => o.Items)
+                    .ThenInclude(i => i.Product)
+                    .OrderByDescending(o => o.OrderDate);
+            }
+
+            return this.context.Orders
+                .Include(o => o.Items)
+                .ThenInclude(i => i.Product)
+                .Where(o => o.User == user)
+                .OrderByDescending(o => o.OrderDate);
+        }
+
+    }
+}
